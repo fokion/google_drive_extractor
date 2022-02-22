ca66502d3f75f03d164b839f6d698205edb374c0
Fokion Sotiropoulos <fokion.s@gmail.com>
2022-02-22 19:02:56 +0000

    fix action that was failing (#2)
    * working on fixing the github actions
    
    * add relative path

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -8,7 +8,7 @@ class ExtractorTest(unittest.TestCase):
     def test_extracting(self):
         path = os.getcwd()
         extractor = ZipExtractor(path,{})
-        extractor.extract_zip_to_destination(os.path.join(path,"takeout-202003022105-001.zip"))
+        extractor.extract_zip_to_destination(os.path.join(path,"./tests/takeout-202003022105-001.zip"))
         exported_file=os.path.join(path,"test.jpg")
         self.assertTrue(os.path.isfile(exported_file))
         os.remove(exported_file)
@@ -16,7 +16,7 @@ class ExtractorTest(unittest.TestCase):
     def test_extracting_extra_extension(self):
         path = os.getcwd()
         extractor = ZipExtractor(path, {".json"})
-        extractor.extract_zip_to_destination(os.path.join(path, "takeout-202003022105-001.zip"))
+        extractor.extract_zip_to_destination(os.path.join(path, "./tests/takeout-202003022105-001.zip"))
         exported_file_image = os.path.join(path, "test.jpg")
         exported_file_json = os.path.join(path, "test.json")
         self.assertTrue(os.path.isfile(exported_file_image))
